259f7d180b016c88f7ecd31c45a7c19f38d2fb44
Yuta123456 <yuuta09090530@icloud.com>
2023-08-03 16:14:25 +0900

    dataset作成

--- learning/Encoder/PokemonEncoder.py
+++ learning/Encoder/PokemonEncoder.py
@@ -9,21 +9,6 @@ import json
 
 from learning.Entity.Pokemon import Pokemon
 
-"""
-画像処理のモデル
-"""
-
-# class ImageEncoder(nn.Module):
-#     def __init__(self, embedding_size):
-#         super(ImageEncoder, self).__init__()
-#         self.resnet50 = models.resnet50(pretrained=True)
-#         self.fc = nn.Linear(self.resnet50.fc.out_features, embedding_size)
-
-#     def forward(self, x):
-#         x = self.resnet50(x)
-#         x = self.fc(x)
-#         return x
-
 
 class PokemonEncoder(nn.Module):
     def __init__(self, device):
